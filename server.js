4ab627d5f2c0f54e02d557751c615c85da2ce774
atharv <atharv@gmail.com>
2026-05-02 22:22:01 +0530

    theme_update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(helmet());
 app.use(compression({ level: 9, threshold: 1024 }));
 
 // Cache static assets for 1 year (31536000 seconds)
-app.use(express.static(path.join(__dirname, 'public'), {
+app.use(express.static(path.join(__dirname, 'docs'), {
   maxAge: '1y',
   etag: false
 }));
@@ -34,36 +34,36 @@ app.use((req, res, next) => {
 
 // Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'docs', 'index.html'));
 });
 
 app.get('/project-mobility', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'project-mobility.html'));
+  res.sendFile(path.join(__dirname, 'docs', 'project-mobility.html'));
 });
 
 app.get('/project-gripper', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'project-gripper.html'));
+  res.sendFile(path.join(__dirname, 'docs', 'project-gripper.html'));
 });
 
 app.get('/project-sixstroke', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'project-sixstroke.html'));
+  res.sendFile(path.join(__dirname, 'docs', 'project-sixstroke.html'));
 });
 
 app.get('/project-hvac', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'project-hvac.html'));
+  res.sendFile(path.join(__dirname, 'docs', 'project-hvac.html'));
 });
 
 app.get('/project-battery', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'project-battery.html'));
+  res.sendFile(path.join(__dirname, 'docs', 'project-battery.html'));
 });
 
 app.get('/project-ecocombust', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'project-ecocombust.html'));
+  res.sendFile(path.join(__dirname, 'docs', 'project-ecocombust.html'));
 });
 
 // 404 handler
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.status(404).sendFile(path.join(__dirname, 'docs', 'index.html'));
 });
 
 // Error handling
